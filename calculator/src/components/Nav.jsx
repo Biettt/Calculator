8789b08a8c32ba8730cfc03ce9b99a019631b004
Biettt <rm551237@fiap.com.br>
2023-11-11 14:10:55 -0300

    formatting routes

---
+++ calculator/src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+   
+      <header className="header">
+        <nav className="headerMenu">
+          <ul>
+            <Link to="/" className="tlink">
+              Home
+            </Link>{' '}
+            {''}
+            <Link to="/singup" className="tlink">
+              Cadastro
+            </Link>
+            <Link to="/login" className="tlink">
+               Login
+            </Link>
+            <Link to="/calculator" className="tlink">
+              Calculadora
+            </Link>
+            <Link to="/conversor" className="tlink">
+              Conversor
+            </Link>
+            <Link to="/about-us" className="tlink">
+              Sobre Nós
+            </Link>
+          </ul>
+        </nav>
+      </header>
+  
+  );
+}
+export default Nav;
--- calculator/src/main.jsx
+++ calculator/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './routers/Home.jsx';
+import Error from './components/Error.jsx';
+import Login from './routers/Login.jsx';
+import Signup from './routers/Singup.jsx';
+import Calculator from './routers/Calculator.jsx';
+import Converter from './routers/Converter.jsx';
+import AboutUs from './routers/AboutUs.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/singup', element: <Signup /> },
+      { path: '/login', element: <Login /> },
+      { path: '/calculator', element: <Calculator /> },
+      { path: '/converter', element: <Converter /> },
+      { path: '/about-us', element: <AboutUs /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <>
+    <RouterProvider router={router} />
+  </>,
+);
