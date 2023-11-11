dd3a0fb02f0a72f8047473d0284c9aa36c78d046
Biettt <rm551237@fiap.com.br>
2023-11-11 14:37:45 -0300

    formatting home

--- calculator/src/routers/Home.jsx
+++ calculator/src/routers/Home.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/Home.css';
 
 function Home() {
     return(
         <div className="home">
-            {/* IREI CRIAR O CONTEÚDO DA PAG HOME */}
-
-            {/* IREI ADD O COMPONENTE FOOTER NA PAG HOME */}
-            <footer className='footer'>
-                <p>Atticus</p>
-                <div>
-                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-                        <svg viewBox='0 0 24 24' xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15H8v-1h3v1zm2.5-4a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm2-6h-2c-1.1 0-2 .9-2 2v2h1.5V9.21H15V11h-1v3h2c1.1 0 2-.9 2-2v-3a2 2 0 00-2-2z"/>
-                        </svg>
-                    </a>
-                </div>
-            </footer>
+            <h2>Bem-Vindo à Atticus</h2>
+            <p>Atticus é o lar do projeto "Challenge Sprints", uma proposta da faculdade onde os alunos têm a oportunidade de criar projetos incríveis. Meu projeto foi focado em desenvolver um sistema de calculadora em React, mas com um toque especial.</p>
+            <p>Decidi expandir o projeto incluindo um conversor de moedas que se conecta a uma API externa para fornecer taxas de câmbio em tempo real de várias moedas ao redor do mundo. Isso adicionou um toque de sofisticação e mais utilidade ao meu trabalho.</p>
+            <p>Utilizando JavaScript, HTML, troquei o código CSS por SCSS para a estilização, criei uma experiência de usuário amigável e funcional. Convido você a explorar meu projeto e descobrir como a matemática e as finanças podem se unir de maneira harmoniosa em um ambiente digital.</p>
         </div>
     );
 }
