d152e8d1a883ece6b397c5bf83506d9e2a87114c
Biettt <rm551237@fiap.com.br>
2023-11-11 18:11:56 -0300

    up footer / nav

--- calculator/src/routers/Footer.jsx
+++ calculator/src/routers/Footer.jsx
@@ -8,15 +8,14 @@ function Footer() {
             <div className="footer-content">
                 <div className="contact-info">
                     <h3>Contato</h3>
-                    <p>Telefone: (11) 2222-4444</p>
-                    <p>Endereço: Avenida Paulista, 777</p>
+                    <p>Telefone: (11) 7070-4242 | Endereço: Avenida Paulista, 777</p>
                 </div>
                 <div className="social-media">
                     <h3>Redes Sociais</h3>
                     <p>
                         LinkedIn: <a href='https://linkedin.com/' target='_blank' rel='noopener noreferrer'>Atticus</a> | 
                         Instagram: <a href='https://instagram.com/' target='_blank' rel='noopener noreferrer'>@Atticus</a> | 
-                        Whatsapp: <a href='https://whatsapp.com/' target='_blank' rel='noopener noreferrer'> (11) 9 8888-1111</a> 
+                        Whatsapp: <a href='https://whatsapp.com/' target='_blank' rel='noopener noreferrer'> (11) 9 6060-1717</a> 
                     </p>
                 </div>
             </div>
